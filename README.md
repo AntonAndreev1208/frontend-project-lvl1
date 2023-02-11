95156a5cf8d804abe138d94f1b4b967d2389e14b
Антон Мурашкин <anton@DESKTOP-GG7J7RE.localdomain>
2023-02-11 18:47:58 +0700

    demo commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![Actions Status](https://github.com/AntonAndreev1208/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/AntonAndreev1208/frontend-project-lvl1/actions)
 <a href="https://codeclimate.com/github/AntonAndreev1208/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/5bbcdeb929cefe2d1fce/maintainability" /></a>
   https://asciinema.org/a/OR3ddfAZEjy8Ll2AUa2d50zM4  - brain-even
-  https://asciinema.org/a/Se8LeQha626ctdS1pRSiulDeR    - brain-calc 
+  https://asciinema.org/a/Se8LeQha626ctdS1pRSiulDeR  - brain-calc 
   https://asciinema.org/a/LERVgkjzwT1pLekGL9RThYBUK  -  brain-gcd  
   https://asciinema.org/a/OUWrE2zUAVTM7u1ABXf2QRiqs  - brain-progression
-  https://asciinema.org/a/hIqjNI8kiOpB3MrmEQPtR8dL0   - brain-prime 
+  https://asciinema.org/a/jP7xUZgEUPAZhIAvCkcYNL14B  - brain-prime 
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,22 +4,30 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Answer "yes" if the given number is prime. Otherwise answer "no".');
 console.log("Let's start the game!");
 
 let correctAnswerCount = 0;
+
 while (correctAnswerCount < 3) {
   const number = Math.floor(Math.random() * 100) + 1;
   console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
 
-  if (answer !== 'yes' && answer !== 'no') {
-    console.log(`"${answer}" is wrong answer ;(. Correct answer was 'no' or 'yes'.`);
-    console.log(`Let's try again, ${userName}!`);
-    break;
+  let isPrime = true;
+  if (number === 1) {
+    isPrime = false;
+  } else {
+    for (let i = 2; i <= number / 2; i = +1) {
+      if (number % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
   }
 
-  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = isPrime ? 'yes' : 'no';
+
   if (answer === correctAnswer) {
     console.log('Correct!');
     correctAnswerCount += 1;
